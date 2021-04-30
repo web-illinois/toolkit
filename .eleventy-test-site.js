43fe32bfbfbf602a4541fd9ca632c7288ada4195
bryanjonker <jonker@illinois.edu>
2021-04-29 19:39:40 -0500

    Adding CI to load files to test site

---
+++ .eleventy-test-site.js
@@ -0,0 +1,25 @@
+const glob = require('fast-glob');
+const path = require('path');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setTemplateFormats(['html', 'md', 'njk', '11ty.js', 'css']);
+  eleventyConfig.addPassthroughCopy({ "dist": "." });
+
+  eleventyConfig.addShortcode("addStaticImages", function () {
+    let info = '<h2>Web Toolkit reference images</h2><ul>';
+    glob.sync('tests/_reference/*.png').forEach(file => {
+      var filename = path.basename(file);
+      var filepath = 'static-images/' + filename;
+       info = info + `<li><a href="${filepath}">${filename}</a></li>`;
+    });
+    info = info + '</ul>';
+    return info;
+  });
+
+  return {
+    dir: {
+      input: "tests",
+      output: "var/full-site-deploy"
+    }
+  };
+};
