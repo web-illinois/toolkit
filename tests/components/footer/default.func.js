eee9c8a2a4fb43dc4a311d8c04c6fb34bfa774e0
Matt Sharkey <msharkey@illinois.edu>
2021-06-15 13:54:57 -0500

    Test component slots are present

---
+++ tests/components/footer/default.func.js
@@ -0,0 +1,42 @@
+const util = require('../../tests.util');
+
+async function getDefaultSlot(tag) {
+  return page.evaluate(tag => {
+    return document.querySelector(tag).shadowRoot.querySelector("slot:not([name])");
+  }, tag);
+}
+
+async function getSlot(tag, name) {
+  return page.evaluate((tag, name) => {
+    return document.querySelector(tag).shadowRoot.querySelector(`slot[name='${name}']`);
+  }, tag, name);
+}
+
+beforeEach(async () => {
+  await page.goto(util.testUrl(__filename))
+});
+
+test('has default slot', async () => {
+  const slot = getDefaultSlot('il-footer');
+  expect(slot).not.toBeNull();
+});
+
+test('has contact slot', async () => {
+  const slot = await getSlot('il-footer', 'contact');
+  expect(slot).not.toBeNull();
+});
+
+test('has social slot', async () => {
+  const slot = await getSlot('il-footer', 'social');
+  expect(slot).not.toBeNull();
+});
+
+test('has parent slot', async () => {
+  const slot = await getSlot('il-footer', 'parent');
+  expect(slot).not.toBeNull();
+});
+
+test('has links slot', async () => {
+  const slot = await getSlot('il-footer', 'links');
+  expect(slot).not.toBeNull();
+});
