53563ff4427ad5c40ce960883bd8cfeec143bec8
Matt Sharkey <msharkey@illinois.edu>
2021-07-30 00:28:12 -0500

    Replace breadcrumb separator with chevron icon
    Fixes #148

--- src/js/components/breadcrumbs-page.css.js
+++ src/js/components/breadcrumbs-page.css.js
@@ -4,12 +4,29 @@ export default css`
   :host {
     counter-increment: breadcrumb;
   }
+  :host(*:last-of-type) .separator {
+    display: none;
+  }
   li {
     display: inline;
     color: #252525;
+    vertical-align: baseline;
   }
   li.current {
     font-weight: 700;
+  }
+  .separator {
+    position: relative;
+    display: inline-block;
+    height: 8px;
+    padding: 0 4px;
+  }
+  .separator svg {
+    position: relative;
+    display: block;
+    width: 100%;
+    height: 100%;
+    fill: var(--il-blue);
   }
   a {
     color: var(--il-link-color);
--- src/js/components/breadcrumbs.css.js
+++ src/js/components/breadcrumbs.css.js
@@ -11,10 +11,4 @@ export default css`
     padding: 0;
     font: 400 14px/32px var(--il-source-sans);
   }
-  ::slotted(il-breadcrumbs-page)::after {
-    content: var(--il-breadcrumbs-separator);
-  }
-  ::slotted(il-breadcrumbs-page:last-of-type)::after {
-    content: "";
-  }
 `;
