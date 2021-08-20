67028600387d00881cd7aac9c0827828f286f8e3
Bryan Jonker <jonker@illinois.edu>
2021-08-20 17:14:00 -0500

    Changed code to use data-il instead of web component

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 import '../css/components.scss';
 
+import './components/accordion';
+import './components/link';
 import './components/breadcrumbs';
 import './components/breadcrumbs-page';
 import './components/back-to-top';
 import './components/footer';
-import './components/footer-link';
 import './components/header';
 import './components/hero';
 import './components/nav';
@@ -14,4 +15,4 @@ import './components/search';
 import './components/section-nav';
 import './components/unit-wordmark';
 import './components/imagefeature';
-import './components/accordion';
+
