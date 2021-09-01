93711885d02d97c2ad7eb7f9786eb1e1e3b44095
Matt Sharkey <msharkey@illinois.edu>
2021-09-01 12:58:57 -0500

    Fix section navigation breakpoints
    Fixes #168, #169, #171

--- src/js/components/section-nav.js
+++ src/js/components/section-nav.js
@@ -1,10 +1,10 @@
-import {LitElement, html, css} from 'lit';
+import { LitElement, html, css } from 'lit';
 
 class SectionNavigation extends LitElement {
   static get properties() {
     return {
-      compact: {type: Boolean, attribute: true, default: false, reflect: true},
-      expanded: {type: Boolean, attribute: false}
+      compact: { type: Boolean, attribute: true, default: false, reflect: true },
+      expanded: { type: Boolean, attribute: false }
     };
   }
 
@@ -111,22 +111,23 @@ nav.compact.expanded .menu {
   }
 
   handleWindowResize() {
-    this.compact = !window.matchMedia("(min-width: 767px)").matches;
+    this.compact = !window.matchMedia("(min-width: 992px)").matches;
   }
 
   render() {
     const mode = this.compact ? 'compact' : 'full';
-    const expanded = this.expanded ? 'expanded': 'collapsed';
+    const expanded = this.expanded ? 'expanded' : 'collapsed';
     return html`
       <nav class=${mode + ' ' + expanded}>
         <div class="header">
-            <h2>In this section</h2>
-            <button class="il-section-nav__toggle" @click=${this.handleToggleClick}>
-              Toggle section navigation menu
-              <svg aria-label="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" role="presentation">
-                <path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"/>
-              </svg>
-            </button>
+          <h2>In this section</h2>
+          <button class="il-section-nav__toggle" @click=${this.handleToggleClick}>
+            Toggle section navigation menu
+            <svg aria-label="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" role="presentation">
+              <path
+                d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z" />
+            </svg>
+          </button>
         </div>
         <div class="menu">
           <slot></slot>
