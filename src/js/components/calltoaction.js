f53fb43917e1abfbf317b53fea348f7fe68661cf
Bryan Jonker <jonker@illinois.edu>
2021-10-11 09:20:52 -0500

    Background and alignment

--- src/js/components/calltoaction.js
+++ src/js/components/calltoaction.js
@@ -1,12 +1,12 @@
-import {LitElement, html, css} from 'lit';
+import { LitElement, html, css } from 'lit';
 
 class CallToAction extends LitElement {
 
   static get properties() {
     return {
-        alignment: {type: String, attribute: true},
-        background: {type: String, attribute: true},
-        width: {type: String, attribute: true},
+      alignment: { type: String, attribute: true },
+      background: { type: String, attribute: true },
+      width: { type: String, attribute: true },
     };
   }
 
@@ -16,18 +16,41 @@ class CallToAction extends LitElement {
       display: flex;
       align-items: center;
       padding: 2.813rem 0;
-      width: 100%;
       max-width: var(--il-content-max-width);
       margin: auto;
     }
-    .il-calltoaction .il-calltoaction-body {
-      max-width: 50rem;
-      margin: auto;
+    .il-calltoaction.gradient {
+      color: white;
+      background: linear-gradient(180deg, var(--il-industrial-blue) 0%, var(--il-blue) 100%);
+    }
+    .il-calltoaction.orange {
+      color: white;
+      background: var(--il-orange);
+    }
+    .il-calltoaction.orangegradient {
+        color: white;
+        background: linear-gradient(180deg, var(--il-orange) 0%, var(--il-altgeld) 100%);
+    }
+    .il-calltoaction.solid {
+        color: white;
+        background: var(--il-blue);
+    }
+    .il-calltoaction .il-calltoaction-body, .il-calltoaction .il-calltoaction-icon  {
+      padding-left: 5vw;
+    }
+    .il-calltoaction .il-calltoaction-body.center {
+      text-align: center;
+    }
+    .il-calltoaction .il-calltoaction-body.right {
+      text-align: right;
     }
     @media only screen and (max-width: 792px) {
       .il-calltoaction {
         display: block;
         padding: 1.875rem;
+      }
+      .il-calltoaction .il-calltoaction-body, .il-calltoaction .il-calltoaction-icon {
+        padding-left: 0;
       }
     }`;
   }
@@ -41,12 +64,14 @@ class CallToAction extends LitElement {
 
 
   render() {
-
-    var contentClass = (this.background == 'solid' || this.background == 'blue') ? 'content solid' : this.background == 'white' ? 'content' : this.background == 'orange' ? 'content orange' : this.background == 'orangegradient' ? 'content orangegradient' : 'content gradient';
+    var contentClass = (this.background == 'solid' || this.background == 'blue') ? 'solid' : this.background == 'white' ? '' : this.background == 'orange' ? 'orange' : this.background == 'orangegradient' ? 'orangegradient' : 'gradient';
+    var alignmentClass = this.alignment == 'left' ? '' : this.alignment;
+    let widthStyle = this.width == '' ? '' : `width: ${this.width};`;
+    
     return html`
-        <div class="il-calltoaction ${contentClass}">
-            <div class="il-calltoaction-icon"><slot name="icon"></slot></div>
-            <div class="il-calltoaction-body"><slot></slot></div>
+        <div class="il-calltoaction ${contentClass}" style="${widthStyle}">
+            <div class="il-calltoaction-icon ${alignmentClass}"><slot name="icon"></slot></div>
+            <div class="il-calltoaction-body ${alignmentClass}"><slot></slot></div>
         </div>
         `;
   }
