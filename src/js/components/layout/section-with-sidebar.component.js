aed697b005e7b462b6694f5133394c6fbd8ce596
Matt Sharkey <msharkey@illinois.edu>
2021-10-28 12:46:38 -0500

    Section with sidebar styling & responsiveness

--- src/js/components/layout/section-with-sidebar.component.js
+++ src/js/components/layout/section-with-sidebar.component.js
@@ -3,19 +3,42 @@ import styles from './section-with-sidebar.css';
 
 class SectionWithSidebarComponent extends LitElement {
 
+  static get properties() {
+    return {
+      compact: { type: Boolean, attribute: true, default: false, reflect: true }
+    };
+  }
+
   static get styles() {
     return styles;
   }
 
   constructor() {
     super();
+    super();
+    this.compact = false;
+    document.addEventListener('DOMContentLoaded', this.handleDocumentLoaded.bind(this));
+    window.addEventListener('resize', this.handleWindowResize.bind(this));
+  }
+
+  handleDocumentLoaded(evt) {
+    this.handleWindowResize();
+  }
+
+  handleWindowResize() {
+    this.compact = !window.matchMedia("(min-width: 992px)").matches;
   }
 
   render() {
+    const mode = this.compact ? 'compact': 'full';
     return html`
-<section>
-  <slot name="sidebar"></slot>
-  <slot></slot>
+<section class=${mode}>
+  <div class="sidebar">
+    <slot name="sidebar"></slot>
+  </div>
+  <div class="main">
+    <slot></slot>
+  </div>
 </section>
     `;
   }
