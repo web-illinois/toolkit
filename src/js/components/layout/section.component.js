49be6cb57d0fa2edb275895f554dc8810c5fcea1
Matt Sharkey <msharkey@illinois.edu>
2021-10-28 13:53:05 -0500

    Layout sections

--- src/js/components/layout/section.component.js
+++ src/js/components/layout/section.component.js
@@ -3,17 +3,25 @@ import styles from './section.css';
 
 class SectionComponent extends LitElement {
 
+  static get properties() {
+    return {
+      width: { type: String, attribute: true, default: 'fixed', reflect: true }
+    };
+  }
+
   static get styles() {
     return styles;
   }
 
   constructor() {
     super();
+    this.width = 'fixed';
   }
 
   render() {
+    const width = `${this.width}-width`;
     return html`
-<section>
+<section class=${width}>
   <slot></slot>
 </section>
     `;
--- src/js/components/layout/section.css.js
+++ src/js/components/layout/section.css.js
@@ -2,4 +2,28 @@ import { css } from 'lit';
 
 export default css`
 
+section {
+  position: relative;
+  margin: 0;
+  padding: 0;
+}
+
+section.fixed-width {
+  padding: 40px 20px;
+}
+
+@media (min-width: 850px) {
+  section.fixed-width {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+}
+
+@media (min-width: 1200px) {
+  section.fixed-width {
+    padding-left: calc(50vw - 570px);
+    padding-right: calc(50vw - 570px);
+  }
+}
+
 `;
