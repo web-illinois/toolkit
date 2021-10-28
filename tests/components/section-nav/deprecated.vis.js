0815a71d2fe205148aed5154e1af2a7b705a6c1d
Matt Sharkey <msharkey@illinois.edu>
2021-10-28 15:51:33 -0500

    Deprecated CSS layout for section nav

--- tests/components/section-nav/deprecated.vis.js
+++ tests/components/section-nav/deprecated.vis.js
@@ -5,12 +5,12 @@ const url = util.testUrl(__filename);
 module.exports = (viewports) => {
   return [
     {
-      url, 'label': 'section-nav',
+      url, 'label': 'section-nav-css-layout',
       'caption': 'Default state',
       'viewports': [viewports.desktop, viewports.iphone, viewports.hdtv]
     },
     {
-      url, 'label': 'section-nav-hover',
+      url, 'label': 'section-nav-css-layout-hover',
       'caption': 'Hover over link',
       'viewports': [viewports.desktop, viewports.hdtv],
       'onReady': async (page, scenario) => {
@@ -18,7 +18,7 @@ module.exports = (viewports) => {
       }
     },
     {
-      url, 'label': 'section-nav-focus',
+      url, 'label': 'section-nav-css-layout-focus',
       'caption': 'Link has focus',
       'viewports': [viewports.desktop, viewports.hdtv],
       'onReady': async (page, scenario) => {
