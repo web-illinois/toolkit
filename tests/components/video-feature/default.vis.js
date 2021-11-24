73daaa4f453a72dbec06053bdb7d0a5b453d0726
Bryan Jonker <jonker@illinois.edu>
2021-11-24 10:28:30 -0600

    Update images, fix release, Melissa changes

--- tests/components/video-feature/default.vis.js
+++ tests/components/video-feature/default.vis.js
@@ -6,12 +6,7 @@ module.exports = (viewports) => {
   return [
     {
       url, label: "videofeature-default",
-      viewports: [viewports.desktop, viewports.iphone, viewports.hdtv],
-      'onReady': async (page, scenario) => {
-        await page.evaluate(() => document.querySelectorAll('il-video-feature').forEach(v => {
-          v.shadowRoot.querySelector('iframe').style.visibility = 'hidden';
-        }));
-      }
+      viewports: [viewports.desktop, viewports.iphone, viewports.hdtv]
     }
   ]
 }
