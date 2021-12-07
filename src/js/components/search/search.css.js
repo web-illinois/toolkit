dd7d23f5f741fd18be03b3ecaf5a2796a0dfd48b
Matt Sharkey <msharkey@illinois.edu>
2021-12-07 09:28:56 -0600

    Search: extract styles, rename attributes
    Separate component code & styles
    Rename "searchname" attribute to "name" (fixes #189)

---
+++ src/js/components/search/search.css.js
@@ -0,0 +1,58 @@
+import {css} from 'lit';
+
+export default css`
+form {
+  display: grid;
+  height: 40px;
+  grid-template-columns: auto 60px;
+  grid-gap: 2px;
+  border: 2px solid var(--il-cloud-3);
+  background-color: var(--il-cloud-3);
+  margin-left: 10px;
+  border-radius: 5px;
+}
+form.input-has-focus {
+  border-color: var(--il-industrial-blue);
+}
+label {
+  display: none;
+}
+input {
+  margin: 0;
+  padding: 5px;
+  border: 0;
+  font: 16px/18px var(--il-source-sans);
+  background-color: white;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  -webkit-appearance: none;
+}
+input:focus {
+  outline: 0;
+}
+button {
+  position: relative;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  background-color: white;
+  color: #606060;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+}
+button:focus {
+  outline: 0px solid var(--il-industrial-blue);
+  box-shadow: 0 0 0 2px var(--il-industrial-blue);
+  color: var(--il-industrial-blue);
+}
+button svg {
+  display: block;
+  position: absolute;
+  left: calc(50% - 10px);
+  top: calc(50% - 10px);
+  width: 20px;
+  height: 20px;
+  fill: currentcolor;
+}
+`;
