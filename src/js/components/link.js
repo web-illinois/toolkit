7d25368f40f6ec0c164172499215312e0d1e8c9b
Bryan Jonker <jonker@illinois.edu>
2022-02-14 10:09:05 -0600

    Issue #310 and #313

--- src/js/components/link.js
+++ src/js/components/link.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let data_il_anchorlinks = {
         privacy: { href: 'https://www.vpaa.uillinois.edu/resources/web_privacy', text: 'Privacy Policy' },
         accessibility: { href: 'https://illinois.edu/resources/website/accessibility.html', text: 'Accessibility' },
-        copyright: { href: 'https://illinois.edu/resources/website/copyright.html', text: 'Copyright &#169; 2021' },
+        copyright: { href: 'https://illinois.edu/resources/website/copyright.html', text: 'Copyright &#169; 2022' },
         cookies: { text: 'About Cookies', id: 'ot-sdk-btn', class: 'ot-sdk-show-settings' }
     };
 
