9f5ba459fd7ca2e3c3954c3036dc25e05bacf962
Matt Sharkey <msharkey@illinois.edu>
2022-05-25 13:18:42 -0500

    New card component

--- src/js/components/card/card.css.js
+++ src/js/components/card/card.css.js
@@ -1,29 +1,4 @@
 import { css } from 'lit';
 
 export default css`
-    article {
-        display: block;
-        border: 1px solid var(--il-gray-2);
-        border-bottom: 3px solid var(--il-orange);
-        margin: var(--il-clickable-card-margin);
-        text-decoration: none;
-        color: var(--il-text-color);
-        background: var(--il-background-color);
-    }
-    article.blue {
-      --il-text-color: white;
-      --il-background-color: var(--il-blue);
-    }
-    img {
-        width: 100%;
-    }
-    div.text {
-      padding: 1.75rem 1.875rem 2.8rem 1.875rem;
-    }
-    div.text.nopicture {
-      padding: 2.8rem 1.875rem;
-    }
-    article a {
-      text-decoration: none;
-    }
 `;
