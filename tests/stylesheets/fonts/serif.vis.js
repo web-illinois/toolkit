92733b2863619634828a330da1fb543893c6d372
Matt Sharkey <msharkey@illinois.edu>
2022-10-11 15:13:59 -0500

    Update serif and condensed fonts

--- tests/stylesheets/fonts/serif.vis.js
+++ tests/stylesheets/fonts/serif.vis.js
@@ -3,7 +3,7 @@ const util = require('../../tests.util');
 module.exports = (viewports) => {
   return [
     {
-      'label': 'fonts-source-serif',
+      'label': 'fonts-serif',
       'url': util.testUrl(__filename),
       'viewports': [viewports.desktop]
     }
