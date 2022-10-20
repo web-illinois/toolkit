a7c392a8946c3972a186985b9173f59d0560688d
Bryan Jonker <jonker@illinois.edu>
2022-10-20 12:31:09 -0500

    Removed unneeded media query

--- src/js/components/video/video.css.js
+++ src/js/components/video/video.css.js
@@ -6,14 +6,4 @@ export default css`
         padding-bottom: var(--il-video-aspectratio); 
         height: 0;
     }
-    @media (min-width: 900px) {
-        .videowrapper {
-            padding-bottom: 100%; 
-        }
-    }
-    @media (min-width: 1450px) {
-        .videowrapper {
-            padding-bottom: var(--il-video-aspectratio); 
-        }
-    }
 `;
