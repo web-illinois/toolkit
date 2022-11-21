3a162536b95b58fd087accca377a7473f04eeb40
Matt Sharkey <msharkey@illinois.edu>
2022-11-20 23:13:44 -0600

    Hero component tests

--- tests/_lib/a11y.js
+++ tests/_lib/a11y.js
@@ -1,5 +1,9 @@
 const colors = require('../../api/colors.json');
 
+function debug(data) {
+  console.debug(JSON.stringify(data, null, 2));
+}
+
 function displayColor(hex) {
   let color = hex;
   const colorName = Object.keys(colors).find(n => colors[n] === hex);
@@ -28,14 +32,25 @@ function notToContainViolations(results) {
         let message = 'The following contrast violations were found:';
         results.violations.forEach(violation => {
           violation.nodes.forEach(node => {
-            const actualRatio = node.any[0].data.contrastRatio;
-            const requiredRatio = node.any[0].data.expectedContrastRatio;
-            message += `\n  * Contrast ratio is ${actualRatio} (should be ${requiredRatio})`;
-            message += `\n    Node: ${node.target[0]}`;
-            message += `\n    Foreground color: ${displayColor(node.any[0].data.fgColor)}`;
-            message += `\n    Background color: ${displayColor(node.any[0].data.bgColor)}`;
-            message += `\n    Font size: ${node.any[0].data.fontSize}`;
-            message += `\n    Font weight: ${node.any[0].data.fontWeight}`;
+            const data = node.any[0].data;
+            const actualRatio = data.contrastRatio;
+            const requiredRatio = data.expectedContrastRatio;
+            if (data.messageKey === 'fgOnShadowColor') {
+              message += `\n  * Insufficient contrast between foreground and shadow`
+              message += `\n    Node: ${node.target[0]}`;
+              message += `\n    Contrast: ${actualRatio} (should be ${requiredRatio})`;
+              message += `\n    Foreground color: ${displayColor(data.fgColor)}`;
+              message += `\n    Shadow color: ${displayColor(data.shadowColor)}`;
+            }
+            else {
+              message += `\n  * Insufficient contrast between foreground and background`
+              message += `\n    Node: ${node.target[0]}`;
+              message += `\n    Contrast: ${actualRatio} (should be ${requiredRatio})`;
+              message += `\n    Foreground color: ${displayColor(node.any[0].data.fgColor)}`;
+              message += `\n    Background color: ${displayColor(node.any[0].data.bgColor)}`;
+            }
+            message += `\n    Font size: ${data.fontSize}`;
+            message += `\n    Font weight: ${data.fontWeight}`;
           })
         })
         return message;
