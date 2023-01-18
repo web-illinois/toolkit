8c64ed30c00d71e3fe2563966bb4ecba7db89452
Bryan Jonker <jonker@illinois.edu>
2023-01-18 16:57:30 -0600

    Adding introduction

---
+++ src/js/components/introduction/introduction.css.js
@@ -0,0 +1,20 @@
+import { css } from 'lit';
+
+export default css`
+.heading {
+    font-size: 2.25rem;
+    line-height: 2.625rem;
+    font-style: oblique;
+    margin-bottom: 0.9375rem;
+    max-width: var(--il-content-max-width);
+    margin: 0 auto;
+}
+
+.introduction {
+    font-size: 1.375rem;
+    line-height: 1.75rem;
+    margin-bottom: 0.9375rem;
+    max-width: var(--il-content-max-width);
+    margin: 0 auto;
+}
+`;
