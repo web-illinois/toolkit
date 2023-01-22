878275354f21962891baa3c21e2020566b18f429
Matt Sharkey <msharkey@illinois.edu>
2023-01-22 12:04:38 -0600

    Adjust sizing of footer identity

--- src/js/components/footer/footer.component.js
+++ src/js/components/footer/footer.component.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit';
+import {LitElement, html, css} from 'lit';
 import styles from './footer.css';
 import wordmark from './wordmark.html';
 import campusLinks from './campus-links.json';
@@ -14,42 +14,58 @@ class Footer extends LitElement {
 
   render() {
     return html`
-        <div class="footer">
-            <div class="site-footer section">
-                <div class="section-inner">
-                    <slot name="identity"></slot>
-                    <slot name="social"></slot>
-                    <slot name="contact"></slot>
-                    <slot></slot>
-                </div>
+      <div class="footer">
+        <div class="site-footer section">
+          <div class="section-inner">
+            <div class="site-footer__top">
+              <div class="site-footer__top-left">
+                <slot name="identity"></slot>
+                <slot name="social"></slot>
+              </div>
+              <div class="site-footer__top-right">
+                <slot name="buttons"></slot>
+              </div>
             </div>
-            <div class="campus-footer section">
-                <div class="section-inner">
-                    <div class="campus-wordmark">
-                        ${wordmark}
-                    </div>
-                    <ul class="links">
-                        ${campusLinks.map(l => html`<li><a href=${l.url}>${l.label}</a></li>`)}
-                    </ul>
-                </div>
+            <div class="site-footer__bottom">
+              <div class="site-footer__bottom-left">
+                <slot name="contact"></slot>
+              </div>
+              <div class="site-footer__bottom-right">
+                <slot></slot>
+              </div>
             </div>
-            <div class="links-section section">
-                <div class="section-inner">
-                    <slot name="links">
-                        <nav class="il-footer-links" aria-label="Legal notices">
-                            <ul>
-                                <li><button data-il="cookies"></button></li>
-                                <li><a data-il="privacy"></a></li>
-                                <li><a data-il="copyright"></a></li>
-                                <li><a data-il="accessibility"></a></li>
-                                <li><a href="#">College Bylaws</a></li>
-                                <li><a href="#">Login</a></li>
-                            </ul>
-                        </nav>
-                    </slot>
-                </div>
-            </div>
+          </div>
         </div>
+        <div class="campus-footer section">
+          <div class="section-inner">
+            <div class="campus-wordmark">
+              ${wordmark}
+            </div>
+            <ul class="links">
+              ${campusLinks.map(l => html`
+                <li><a href=${l.url}>${l.label}</a></li>`)}
+            </ul>
+          </div>
+        </div>
+        <div class="links-section section">
+          <div class="section-inner">
+            <slot name="links">
+              <nav class="il-footer-links" aria-label="Legal notices">
+                <ul>
+                  <li>
+                    <button data-il="cookies"></button>
+                  </li>
+                  <li><a data-il="privacy"></a></li>
+                  <li><a data-il="copyright"></a></li>
+                  <li><a data-il="accessibility"></a></li>
+                  <li><a href="#">College Bylaws</a></li>
+                  <li><a href="#">Login</a></li>
+                </ul>
+              </nav>
+            </slot>
+          </div>
+        </div>
+      </div>
     `;
   }
 }
--- src/js/components/footer/footer.css.js
+++ src/js/components/footer/footer.css.js
@@ -8,12 +8,26 @@ export default css`
     border-top: 5px solid var(--il-blue);
     color: var(--il-blue);
   }
+
+  .site-footer__top {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+  .site-footer__bottom {
+    margin-top: 1em;
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+  }
+  
   .campus-footer {
     padding: 40px 0;
     background-color: var(--il-blue);
     border-top: 5px solid var(--il-orange);
     color: white;
   }
+
   .campus-footer .links {
     margin: 30px 0 0;
     padding: 0;
@@ -21,103 +35,117 @@ export default css`
     column-count: 3;
     column-gap: 30px;
   }
+
   .campus-footer .links li {
     margin: 0 0 .8em;
     padding: 0;
     list-style: none;
     font-size: 18px;
   }
+
   .campus-footer .links a {
     color: white;
     text-decoration: none;
   }
-  
-.footer {
-  font-size: 1.5rem;
-  line-height: 1.5rem;
-  font-family: var(--il-font-sans);
-  background-color: var(--il-blue);
-  color: white;
-}
 
-.section {
-  padding-left: var(--il-content-margin);
-  padding-right: var(--il-content-margin);
-}
-.section-inner {
-  max-width: var(--il-content-max-width);
-  margin-left: auto;
-  margin-right: auto;
-}
+  .footer {
+    font-size: 1.5rem;
+    line-height: 1.5rem;
+    font-family: var(--il-font-sans);
+    background-color: var(--il-blue);
+    color: white;
+  }
 
-.links-section {
-  border-top: 5px solid var(--il-alma-mater);
-}
+  .section {
+    padding-left: var(--il-content-margin);
+    padding-right: var(--il-content-margin);
+  }
+
+  .section-inner {
+    max-width: var(--il-content-max-width);
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .links-section {
+    border-top: 5px solid var(--il-alma-mater);
+  }
 
 
-.main {
-  padding-top: 30px;
-  padding-bottom: 19px;
-}
+  .main {
+    padding-top: 30px;
+    padding-bottom: 19px;
+  }
 
-.main .section-inner {
-  box-sizing: border-box;
-  display: grid;
-  grid-gap: 0 120px;
-}
-
-@media (max-width: 991px) {
   .main .section-inner {
+    box-sizing: border-box;
+    display: grid;
+    grid-gap: 0 120px;
+  }
+
+  @media (max-width: 991px) {
+    .main .section-inner {
       grid-template-columns: 1fr;
       grid-template-rows: auto;
       grid-template-areas: "info" "content";
+    }
   }
-}
-@media (min-width: 992px) {
-  .main .section-inner {
+  @media (min-width: 992px) {
+    .main .section-inner {
       grid-template-columns: repeat(3, 1fr);
       grid-template-areas: "info content content";
+    }
   }
-}
-.info {
-  grid-area: info;
-}
-.content {
-  grid-area: content;
-}
-.campus-wordmark {
-  width: 100%;
-  max-width: 235px;
-  height: 61px;
-}
-.campus-wordmark a {
-  display: block;
-  text-decoration: none;
-  padding: 5px;
-}
-.campus-wordmark a:focus, .campus-wordmark a:hover {
-  outline: rgb(255, 255, 255) dotted 2px;
-}
-.campus-wordmark svg {
-  display: block;
-}  
-.block-i__outline {
-  fill: white;
-}
-.block-i__fill {
-  fill: var(--il-orange);
-}
-.wordmark__text {
-  fill: white;
-}
-.social {
-  clear: left;
-  margin-top: 25px;
-}
-.links .section-inner {
-  padding-top: 10px;
-  padding-bottom: 10px;
-}
+
+  .info {
+    grid-area: info;
+  }
+
+  .content {
+    grid-area: content;
+  }
+
+  .campus-wordmark {
+    width: 100%;
+    max-width: 235px;
+    height: 61px;
+  }
+
+  .campus-wordmark a {
+    display: block;
+    text-decoration: none;
+    padding: 5px;
+  }
+
+  .campus-wordmark a:focus, .campus-wordmark a:hover {
+    outline: rgb(255, 255, 255) dotted 2px;
+  }
+
+  .campus-wordmark svg {
+    display: block;
+  }
+
+  .block-i__outline {
+    fill: white;
+  }
+
+  .block-i__fill {
+    fill: var(--il-orange);
+  }
+
+  .wordmark__text {
+    fill: white;
+  }
+
+  .social {
+    clear: left;
+    margin-top: 25px;
+  }
+
+  .links .section-inner {
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
 
 
 `;
