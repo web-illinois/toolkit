121821df2e164f782f0a28f78346d9e57e8d8fc4
Matt Sharkey <msharkey@illinois.edu>
2023-05-24 18:49:05 -0500

    Theme content colors

--- src/components/content/content.component.js
+++ src/components/content/content.component.js
@@ -1,19 +1,20 @@
-import { LitElement, html } from 'lit';
-import styles from "./content.css";
-import "./content.scss";
+import {LitElement, html, css} from 'lit';
+import "./css/index.scss";
 
 class ContentComponent extends LitElement {
 
     static get styles() {
-        return styles;
+        return css`
+          :host {
+            display: block;
+          }`
     }
 
     render() {
         return html`
-<div>
-    <slot></slot>
-</div>
-    `;
+            <div>
+                <slot></slot>
+            </div>`
     }
 }
 
--- src/components/content/content.css.js
+++
@@ -1,7 +0,0 @@
-import {css} from 'lit';
-
-export default css`
-  :host {
-    font: 400 1em/1em var(--il-font-sans);
-  }
-`
