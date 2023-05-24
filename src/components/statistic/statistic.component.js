6c3967b0a17121e29848c231325c06f3c1b2a40f
Matt Sharkey <msharkey@illinois.edu>
2023-05-24 13:58:55 -0500

    Statistic refactor with tests

--- src/components/statistic/statistic.component.js
+++ src/components/statistic/statistic.component.js
@@ -1,48 +1,22 @@
 import { LitElement, html, css } from 'lit';
-import styles from './statistic.css';
 import "./statistic.scss";
 class Statistic extends LitElement {
 
-  static get properties() {
-    return {
-      startAnimation: { type: String, attribute: true },
-      animate: { type: Boolean, attribute: true },
-      originalStat: { type: String, attribute: true },
-      idInfo: { type: String }
-    };
-  }
-
   static get styles() {
-    return styles;
-  }
-
-  constructor() {
-    super();
-    this.animated = false;
-    this.startAnimation = '';
-    this.originalStat = '';
-    this.idInfo = '';
-  }
-
-  addOriginalStat(e) {
-    if (this.originalStat == '') {
-      this.originalStat = e.target.assignedNodes({ flatten: true })[0].innerHTML;
-    }
-  }
-
-  firstUpdated() {
-    this.shadowRoot.querySelector('slot[name="stat"]').addEventListener('slotchange', (e) => this.addOriginalStat(e));
+    return css`
+      :host {
+        display: block;
+        background: var(--il-statistic--background-color);
+        color: var(--il-statistic--color);
+        padding: 1.5em 1.25em 2em;
+        text-align: center;
+        font: 400 var(--il-statistic--font-size) var(--il-font-sans);
+      }
+    `;
   }
 
   render() {
-    this.idInfo = 'statistic-' + (((1 + Math.random()) * 0x10000000) | 0);
-    return html`
-    <p class="il-statistic" id='${this.idInfo}'>
-        <span class="text intro"><slot name="top"></slot></span>
-        <span class="stat"><slot name="stat"></span></slot>
-        <span class="text"><slot></slot></span>
-        <span class="text source"><slot name="source"></slot></span>
-    </p>`;
+    return html`<div id="statistic"><slot></slot></div>`;
   }
 }
 
