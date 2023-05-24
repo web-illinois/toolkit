46754021ca507e5c8bdc247c6d1be31eeb9bcf7b
Matt Sharkey <msharkey@illinois.edu>
2023-05-24 17:38:15 -0500

    Image features

--- src/components/image-feature/image-feature.component.js
+++ src/components/image-feature/image-feature.component.js
@@ -1,117 +1,24 @@
-import { LitElement, html } from 'lit';
+import {LitElement, html} from 'lit';
 import styles from './image-feature.css';
-import Debugger from '../../js/debug';
 import "./image-feature.scss";
 
 class ImageFeatureComponent extends LitElement {
-
-  static get properties() {
-    return {
-        size: {type: String, attribute: true},
-        background: {type: String, attribute: true},
-        src: {type: String, attribute: true},
-        alt: {type: String, attribute: true},
-        align: {type: String, attribute: true}
-    };
-  }
 
   static get styles() {
     return styles;
   }
 
-  constructor() {
-    super();
-    this.size = '';
-    this.src = undefined;
-    this.alt = '';
-    this.background = '';
-    this.align = '';
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    if (this.hasAttribute('align')) {
-      Debugger.warn("il-image-feature: the \"align\" attribute is deprecated. Use alignment classes instead.");
-    }
-    if (this.hasAttribute('alt')) {
-      Debugger.warn("il-image-feature: the \"alt\" attribute is deprecated. Use the image slot instead.");
-    }
-    if (this.hasAttribute('background')) {
-      Debugger.warn("il-image-feature: the \"background\" attribute is deprecated. Use a slotted image element instead.");
-    }
-    if (this.hasAttribute('size')) {
-      Debugger.warn("il-image-feature: the \"size\" attribute is deprecated. Use size classes instead.");
-    }
-    if (this.hasAttribute('src')) {
-      Debugger.warn("il-image-feature: the \"src\" attribute is deprecated. Use the image slot instead.");
-    }
-  }
-
   render() {
-    if (this.previousElementSibling != null && this.previousElementSibling.localName == "il-image-feature" && !this.previousElementSibling.classList.contains('il-align-right') && !this.previousElementSibling.classList.contains('il-align-left') && !this.classList.contains('il-align-right') && !this.classList.contains('il-align-left')) {
-      this.classList.add('il-align-right');
-    }
-
-    if (this.background !== '') {
-      if (this.background == 'solid' || this.background == 'blue') {
-        this.classList.add('il-theme-blue');
-      }
-      else if (this.background == 'orange') {
-        this.classList.add('il-theme-orange');
-      }
-    }
-    if (this.align !== '') {
-      if (this.align == 'left') {
-        this.classList.add('il-align-left');
-      }
-      else if (this.align == 'right') {
-        this.classList.add('il-align-right');
-      }
-    }
-    if (this.size !== '') {
-      if (this.size == 'portrait') {
-        this.classList.add('il-size-xsmall');
-      }
-      else if (this.size == 'small') {
-        this.classList.add('il-size-small');
-      }
-      else if (this.size == 'large') {
-        this.classList.add('il-size-large');
-      }
-    }
-    if (this.src !== undefined) {
-      return html`
-      <div class="imagefeature">
-          <div class="background"><img src='${this.src}' alt='${this.alt}' style='object-fit: cover; position: absolute; width: 100%; height: 100%;'></div>
-          <div class="content">
-            <div class="content-inner">
-              <slot></slot>
-            </div>
-          </div>
-      </div>`;
-    }
-
-    if (this.classList.contains('il-overlay')) {
-      return html`
-      <div class="il-image-feature-with-overlay">
-        <slot name="image"></slot>
-        <div class="il-image-feature-with-overlay-outer">
-          <div class="il-image-feature-with-overlay-inner">
-            <div class="il-image-feature-with-overlay-content">
-              <slot></slot>
-            </div>
-          </div>
-        </div>
-      </div>`;
-    }
-
     return html`
-        <div class="imagefeature">
-            <div class="background"><slot name="image"></slot></div>
-            <div class="content">
-              <div class="content-inner">
-                <slot></slot>
-              </div>
+        <div id="container">
+            <div id="image">
+                <slot name="image"></slot>
+            </div>
+            <div id="main">
+                <div id="background"></div>
+                <div id="content">
+                    <slot></slot>
+                </div>
             </div>
         </div>`;
   }
--- src/components/image-feature/image-feature.css.js
+++ src/components/image-feature/image-feature.css.js
@@ -1,102 +1,189 @@
 import { css } from 'lit';
 
-
 export default css`
-    .imagefeature {
-        display: block;
-        margin: 0 auto;
-        max-width: var(--il-image-feature-max-width);
+
+  #container {
+    display: block;
+    container-type: inline-size;
+    position: relative;
+    overflow: hidden;
+    background: var(--il-image-feature--background);
+  }
+
+  #image {
+    position: relative;
+    width: 100%;
+    overflow: hidden;
+    pointer-events: none;
+  }
+  
+  #image ::slotted(img) {
+    width: 100%;
+    object-fit: cover;
+    object-position: top center;
+  }
+
+  #main {
+    box-sizing: border-box;
+    position: relative;
+    padding: 1em;
+  }
+  
+  ::slotted(il-content) {
+    --il-content-base-font: 1em var(--il-font-sans);
+  }
+
+  #background {
+    position: absolute;
+    left: 1.5em;
+    top: 1.5em;
+    z-index: 1;
+    width: calc(100% - 3em);
+    height: calc(100% - 3em);
+    opacity: .9;
+    pointer-events: none;
+    background: var(--il-image-feature--content-background--background);
+  }
+  
+  @container (min-width: 600px) {
+    #image {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: var(--il-image-feature--image--width);
+      height: 100%;
+      overflow: hidden;
+      pointer-events: none;
     }
-    .imagefeature.right {
-        --il-image-feature-flex-direction: row-reverse;
+
+    #image ::slotted(img) {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      object-position: center;
     }
-    .imagefeature.large {
-        --il-image-feature-flex-grow-image: 2;
+
+    #main {
+      box-sizing: border-box;
+      position: relative;
+      margin-left: 50%;
+      min-height: 40cqw;
+      padding: 3em;
     }
-    .imagefeature.small {
-        --il-image-feature-flex-grow-content: 2;
-    }
-    .imagefeature.portrait {
-        --il-image-feature-flex-grow-content: 4;
-        --il-image-feature-min-height: 10vw;
-    }
-    .imagefeature .background {
-        padding: 0;
-        position: relative;
-        background-position: center; 
-        background-repeat: no-repeat; 
-        background-size: cover;
-        min-height: 310px;
-        color: transparent;
-        display: flex;
-        flex-grow: var(--il-image-feature-flex-grow-image);
-        flex-shrink: 1;
-        flex-basis: 0;
-    }
-    .imagefeature .content {
-        text-align: left;
-        color: var(--il-image-feature-color);
-        background: var(--il-image-feature-background);
-        flex-grow: var(--il-image-feature-flex-grow-content);
-        flex-shrink: 1;
-        flex-basis: 0;
-        height: 100%;
-    }
-    .imagefeature .content .content-inner {
-        padding: var(--il-image-feature-content-padding);
-    }
+  }
+
+  #content {
+    position: relative;
+    z-index: 100;
+  }
+
+
+  .imagefeature {
+    display: block;
+    margin: 0 auto;
+    max-width: var(--il-image-feature-max-width);
+  }
+
+  .imagefeature.right {
+    --il-image-feature-flex-direction: row-reverse;
+  }
+
+  .imagefeature.large {
+    --il-image-feature-flex-grow-image: 2;
+  }
+
+  .imagefeature.small {
+    --il-image-feature-flex-grow-content: 2;
+  }
+
+  .imagefeature.portrait {
+    --il-image-feature-flex-grow-content: 4;
+    --il-image-feature-min-height: 10vw;
+  }
+
+  .imagefeature .background {
+    padding: 0;
+    position: relative;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: 310px;
+    color: transparent;
+    display: flex;
+    flex-grow: var(--il-image-feature-flex-grow-image);
+    flex-shrink: 1;
+    flex-basis: 0;
+  }
+
+  .imagefeature .content {
+    text-align: left;
+    color: var(--il-image-feature-color);
+    flex-grow: var(--il-image-feature-flex-grow-content);
+    flex-shrink: 1;
+    flex-basis: 0;
+    height: 100%;
+  }
+
+  .imagefeature .content .content-inner {
+    padding: var(--il-image-feature-content-padding);
+  }
+
+  .il-image-feature-with-overlay {
+    position: relative;
+  }
+
+  .il-image-feature-with-overlay-inner {
+    padding-top: 75px;
+    padding-bottom: 105px;
+    max-width: var(--il-content-max-width);
+    margin: auto;
+    display: flex;
+    justify-content: var(--il-image-feature-with-overlay-align);
+    color: var(--il-image-feature-color);
+  }
+
+  .il-image-feature-with-overlay-content {
+    width: var(--il-image-feature-with-overlay-width);
+    padding: var(--il-image-feature-with-overlay-padding);
+    background: var(--il-image-feature-with-overlay-background);
+    margin: 0 70px;
+  }
+
+  @media only screen and (max-width: 990px) {
     .il-image-feature-with-overlay {
-        position: relative;
+      background: var(--il-image-feature-with-overlay-background-solid);
+      border: var(--il-image-feature-with-overlay-border);
     }
-    
+
+    .il-image-feature-with-overlay-outer {
+      border-top: var(--il-image-feature-with-overlay-border);
+    }
+
     .il-image-feature-with-overlay-inner {
-        padding-top: 75px;
-        padding-bottom: 105px;
-        max-width: var(--il-content-max-width);
-        margin: auto;
-        display: flex;
-        justify-content: var(--il-image-feature-with-overlay-align);
-        color: var(--il-image-feature-color);
+      margin: 0;
+      padding: 0 30px;
     }
-    
+
     .il-image-feature-with-overlay-content {
-        width: var(--il-image-feature-with-overlay-width);
-        padding: var(--il-image-feature-with-overlay-padding);
-        background: var(--il-image-feature-with-overlay-background);
-        margin: 0 70px;
+      position: initial;
+      width: initial;
+      z-index: initial;
+      padding: 30px 0;
+      background: var(--il-image-feature-with-overlay-background-solid);
+      border: none;
     }
-    
-    @media only screen and (max-width: 990px) {
-        .il-image-feature-with-overlay {
-            background: var(--il-image-feature-with-overlay-background-solid);
-            border: var(--il-image-feature-with-overlay-border);
-        }
-        .il-image-feature-with-overlay-outer {
-            border-top: var(--il-image-feature-with-overlay-border);
-        }
-        .il-image-feature-with-overlay-inner {
-            margin: 0;
-            padding: 0 30px;
-        }
-        .il-image-feature-with-overlay-content {
-            position: initial;
-            width: initial;
-            z-index: initial;
-            padding: 30px 0;
-            background: var(--il-image-feature-with-overlay-background-solid);
-            border: none;
-        }
+  }
+
+  @media (min-width: 767px) {
+    .imagefeature {
+      display: flex;
+      flex-direction: var(--il-image-feature-flex-direction);
+      min-height: var(--il-image-feature-min-height);
     }
-   
-    @media (min-width: 767px) {
-        .imagefeature {
-            display: flex;
-            flex-direction: var(--il-image-feature-flex-direction);
-            min-height: var(--il-image-feature-min-height);
-            background: var(--il-image-feature-background);
-        }
-        .imagefeature .background {
-            min-height: initial;
-        }
+
+    .imagefeature .background {
+      min-height: initial;
     }
+  }
 `;
