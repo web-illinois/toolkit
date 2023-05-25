62a4aae07bcd1894e8b17167190334d26ce24ca3
Matt Sharkey <msharkey@illinois.edu>
2023-05-25 18:34:59 -0500

    Merge breadcrumbs into nav component

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import './css/base.css';
 // Core components
 import "./components/il-content/il-content.component";
 import "./components/il-nav/il-nav.component";
+import "./components/il-nav-section/il-nav-section.component";
 
+// Updated components
 import "./components/il-accordion/il-accordion.component";
 import "./components/il-accordion-panel/il-accordion-panel.component";
 import "./components/il-contact/il-contact.component";
 import "./components/il-header/il-header.component";
 import "./components/il-image-feature/il-image-feature.component";
-import "./components/il-nav-section/il-nav-section.component";
 import "./components/il-page/il-page.component";
 import "./components/il-search/il-search.component";
 
+// Legacy components
 import "./components/back-to-top/back-to-top.component";
-import "./components/breadcrumbs/breadcrumbs.component";
-import "./components/breadcrumbs/page.component";
 import "./components/call-to-action/call-to-action.component";
 import "./components/card/card.component";
 import "./components/directory/card.component";
@@ -27,9 +27,7 @@ import "./components/layout/section-with-sidebar.component";
 import "./components/link/link";
 import "./components/page-title/page-title.component";
 import "./components/quote/quote.component";
-import "./components/section-nav/section-nav.component";
 import './components/statistic/statistic.component';
-import "./components/unit-wordmark/unit-wordmark.component";
 import "./components/vertical-tab/vertical-tab.component";
 import "./components/vertical-tab/panel.component";
 import "./components/video/video.component";
