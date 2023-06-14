0a72f082f4da455545e542c5ec2249b845446ee3
Bryan Jonker <jonker@illinois.edu>
2023-06-14 17:06:21 -0500

    Adding image feature

--- src/components/il-image-feature/il-image-feature.component.js
+++ src/components/il-image-feature/il-image-feature.component.js
@@ -8,14 +8,37 @@ class ImageFeatureComponent extends LitElement {
     return styles;
   }
 
+  
+  static get properties() {
+    return {
+      compact: { default: false, attribute: false }
+    };
+  }
+
+  static hasContainerSupport() {
+    return CSS.supports('container-type', 'inline-size');
+  }
+
+  handleResize(evt) {
+    this.compact = this.offsetWidth < 768;
+  }
+
+  constructor() {
+    super();
+    if (!ImageFeatureComponent.hasContainerSupport()) {
+      console.debug("Image Feature: No support for @container detected: using manual resize");
+      window.addEventListener('load', this.handleResize.bind(this));
+      window.addEventListener('resize', this.handleResize.bind(this));
+    }
+  }
+
   render() {
     return html`
-        <div id="container">
+        <div id="container" ?compact=${this.compact}>
             <div id="image">
                 <slot name="image"></slot>
             </div>
             <div id="main">
-                <div id="background"></div>
                 <div id="content">
                     <slot></slot>
                 </div>
--- src/components/il-image-feature/il-image-feature.styles.js
+++ src/components/il-image-feature/il-image-feature.styles.js
@@ -1,32 +1,45 @@
 import { css } from 'lit';
 
 export default css`
-
   #container {
-    display: block;
-    container-type: inline-size;
+    display: var(--il-image-feature--display, flex);
+    flex-direction: var(--il-image-feature--flex-direction, row);
     position: relative;
     overflow: hidden;
     background: var(--il-image-feature--background);
+    min-height: var(--il-image-feature--min-height);
+    margin: 0 auto;
+    max-width: var(--il-image-feature--max-width);
   }
 
   #image {
-    position: relative;
+    position: var(--il-image-feature--image--position);
     width: 100%;
     overflow: hidden;
     pointer-events: none;
+    display: var(--il-image-feature--image--display);
+    align-items: center;
+    justify-content: center;
+    flex: 1 1 0px;
   }
   
   #image ::slotted(img) {
     width: 100%;
+    height: 100%;
     object-fit: cover;
-    object-position: top center;
+    position: absolute;
+    object-position: center;
   }
 
   #main {
     box-sizing: border-box;
     position: relative;
     padding: 1em;
+    flex-grow: var(--il-image-feature--main--flex-grow);
+    flex-shrink: 1;
+    flex-basis: 0px;
+    max-width: var(--il-image-feature--main--max-width);
+    margin: auto;
   }
   
   #background {
@@ -38,148 +51,43 @@ export default css`
     height: calc(100% - 3em);
     opacity: .9;
     pointer-events: none;
-    background: var(--il-image-feature--content-background--background);
-  }
-  
-  @container (min-width: 600px) {
-    #image {
-      position: absolute;
-      left: 0;
-      top: 0;
-      width: var(--il-image-feature--image--width);
-      height: 100%;
-      overflow: hidden;
-      pointer-events: none;
-    }
-
-    #image ::slotted(img) {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      overflow: hidden;
-      object-position: center;
-    }
-
-    #main {
-      box-sizing: border-box;
-      position: relative;
-      margin-left: 50%;
-      min-height: 40cqw;
-      padding: 3em;
-    }
   }
 
   #content {
+    padding: var(--il-image-feature--content--padding);
     position: relative;
     z-index: 100;
+    opacity: .9;
+    background: var(--il-image-feature--background);
+    margin: var(--il-image-feature--content--margin, auto);
+    width: var(--il-image-feature--content--width, auto);
   }
 
-
-  .imagefeature {
-    display: block;
-    margin: 0 auto;
-    max-width: var(--il-image-feature-max-width);
-  }
-
-  .imagefeature.right {
-    --il-image-feature-flex-direction: row-reverse;
-  }
-
-  .imagefeature.large {
-    --il-image-feature-flex-grow-image: 2;
-  }
-
-  .imagefeature.small {
-    --il-image-feature-flex-grow-content: 2;
-  }
-
-  .imagefeature.portrait {
-    --il-image-feature-flex-grow-content: 4;
-    --il-image-feature-min-height: 10vw;
-  }
-
-  .imagefeature .background {
-    padding: 0;
-    position: relative;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    min-height: 310px;
-    color: transparent;
-    display: flex;
-    flex-grow: var(--il-image-feature-flex-grow-image);
-    flex-shrink: 1;
-    flex-basis: 0;
-  }
-
-  .imagefeature .content {
-    text-align: left;
-    color: var(--il-image-feature-color);
-    flex-grow: var(--il-image-feature-flex-grow-content);
-    flex-shrink: 1;
-    flex-basis: 0;
-    height: 100%;
-  }
-
-  .imagefeature .content .content-inner {
-    padding: var(--il-image-feature-content-padding);
-  }
-
-  .il-image-feature-with-overlay {
-    position: relative;
-  }
-
-  .il-image-feature-with-overlay-inner {
-    padding-top: 75px;
-    padding-bottom: 105px;
-    max-width: var(--il-content-max-width);
-    margin: auto;
-    display: flex;
-    justify-content: var(--il-image-feature-with-overlay-align);
-    color: var(--il-image-feature-color);
-  }
-
-  .il-image-feature-with-overlay-content {
-    width: var(--il-image-feature-with-overlay-width);
-    padding: var(--il-image-feature-with-overlay-padding);
-    background: var(--il-image-feature-with-overlay-background);
-    margin: 0 70px;
-  }
-
-  @media only screen and (max-width: 990px) {
-    .il-image-feature-with-overlay {
-      background: var(--il-image-feature-with-overlay-background-solid);
-      border: var(--il-image-feature-with-overlay-border);
+  @container (max-width: 767px) {
+    #container {
+      display: block;
+      border: var(--il-image-feature--container--border, none);
     }
-
-    .il-image-feature-with-overlay-outer {
-      border-top: var(--il-image-feature-with-overlay-border);
-    }
-
-    .il-image-feature-with-overlay-inner {
-      margin: 0;
-      padding: 0 30px;
-    }
-
-    .il-image-feature-with-overlay-content {
+    #image ::slotted(img) {
       position: initial;
-      width: initial;
-      z-index: initial;
-      padding: 30px 0;
-      background: var(--il-image-feature-with-overlay-background-solid);
-      border: none;
+    }
+    #content {
+      padding: 20px 30px;
+      margin: auto;
+      width: auto;
     }
   }
 
-  @media (min-width: 767px) {
-    .imagefeature {
-      display: flex;
-      flex-direction: var(--il-image-feature-flex-direction);
-      min-height: var(--il-image-feature-min-height);
-    }
-
-    .imagefeature .background {
-      min-height: initial;
-    }
+  #container[compact] {
+    display: block;
+    border: var(--il-image-feature--container--border, none);
+  }
+  #container[compact] #image ::slotted(img) {
+    position: initial;
+  }
+  #container[compact] #content {
+    padding: 20px 30px;
+    margin: auto;
+    width: auto;
   }
 `;
