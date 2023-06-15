7fa45a3c4fb82074f6a52a0b3bd16bb1332130c0
Bryan Jonker <jonker@illinois.edu>
2023-06-15 12:49:54 -0500

    Adding clickable option, moving compact to state

--- src/components/il-image-feature/il-image-feature.component.js
+++ src/components/il-image-feature/il-image-feature.component.js
@@ -11,7 +11,9 @@ class ImageFeatureComponent extends LitElement {
   
   static get properties() {
     return {
-      compact: { default: false, attribute: false }
+      _compact: { default: false, type: Boolean, attribute: false, state: true },
+      linkedby: { default: false, type: String, attribute: true },
+      highlight: {default: false, type: Boolean, attribute: true, reflect: true}
     };
   }
 
@@ -19,8 +21,12 @@ class ImageFeatureComponent extends LitElement {
     return CSS.supports('container-type', 'inline-size');
   }
 
+  static determineClickable() {
+    return CSS.supports('container-type', 'inline-size');
+  }
+
   handleResize(evt) {
-    this.compact = this.offsetWidth < 768;
+    this._compact = this.offsetWidth < 768;
   }
 
   constructor() {
@@ -32,14 +38,57 @@ class ImageFeatureComponent extends LitElement {
     }
   }
 
+  firstUpdated() {
+    if (this.linkedby != '') {
+      let anchor = this.querySelector("#" + this.linkedby);
+      if (anchor != null) {
+        anchor.addEventListener("focus", (event) => { this.highlight = true; });
+        anchor.addEventListener("blur", (event) => { this.highlight = false; });
+      } else {
+        console.debug(`Image Feature: ID ${this.linkedby} not found`);
+        this.linkedby = ''
+      }
+    }
+  }
+
+  _click(e) {
+    let anchor = this.querySelector("#" + this.linkedby);
+    anchor.click();
+  }
+
+  _higlight(e) {
+    this.highlight = true;
+    let anchor = this.querySelector("#" + this.linkedby);
+    anchor.focus();
+  }
+
+  _tonedown(e) {
+    this.highlight = false;
+    let anchor = this.querySelector("#" + this.linkedby);
+    anchor.blur();
+  }
+
   render() {
     if (this.previousElementSibling != null && this.previousElementSibling.localName == "il-image-feature" && 
       !this.previousElementSibling.classList.contains('il-float-right') && !this.previousElementSibling.classList.contains('il-float-left') && 
       !this.classList.contains('il-float-right') && !this.classList.contains('il-float-left')) {
         this.classList.add('il-float-right');
     }
+    if (this.linkedby != '') {
+      return html`
+        <div id="container" ?compact=${this._compact} @click="${this._click}" @mouseover="${this._higlight}" @mouseout="${this._tonedown}">
+          <div id="image">
+            <slot name="image"></slot>
+          </div>
+          <div id="main">
+              <div id="content">
+                  <slot></slot>
+              </div>
+          </div>
+        </div>`;
+    }
     return html`
-        <div id="container" ?compact=${this.compact}>
+        <div id="container" ?compact=${this._compact}>
             <div id="image">
                 <slot name="image"></slot>
             </div>
