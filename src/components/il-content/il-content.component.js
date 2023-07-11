30ca2ae0731fc9394af9db73497414e5dde50fbc
Bryan Jonker <jonker@illinois.edu>
2023-07-11 09:37:12 -0500

    Rough draft information

--- src/components/il-content/il-content.component.js
+++ src/components/il-content/il-content.component.js
@@ -3,18 +3,77 @@ import "./il-content.css";
 
 class ContentComponent extends LitElement {
 
+    static get properties() {
+        return {
+          linkedby: { default: false, type: String, attribute: true }
+        };
+      }
+
+      assignClickableState() {
+        if (this.linkedby && this.linkedby != '') {
+          let anchor = this.getLinkElement();
+          if (anchor != null) {
+            anchor.addEventListener("focus", (event) => { this.setHighlight(); });
+            anchor.addEventListener("blur", (event) => { this.removeHighlight(); });
+          }
+        }
+      }
+
     static get styles() {
         return css`
           :host {
             display: block;
+          }
+          div {
+            display: var(--il-content--display);
+            flex-flow: column nowrap;
+            height: 100%;
           }`
     }
 
+    static get highlightAttribute() {
+      return 'data-il-content-highlight';
+    }
+
+     getLinkElement() {
+       return document.getElementById(this.linkedby);
+     }
+
+     _click(e) {
+       this.getLinkElement().click();
+     }
+
+     setHighlight() {
+       return this.setAttribute(ContentComponent.highlightAttribute, true);
+     }
+
+     removeHighlight() {
+       return this.removeAttribute(ContentComponent.highlightAttribute);
+     }
+
+     _highlight(e) {
+       this.setHighlight();
+       this.getLinkElement().focus();
+     }
+
+     _tonedown(e) {
+       this.removeHighlight();
+       this.getLinkElement().blur();
+     }
+
     render() {
-        return html`
+        this.assignClickableState();
+        if (this.linkedby && this.linkedby != '') {
+            return html`
+            <div @click="${this._click}" @mouseover="${this._highlight}" @mouseout="${this._tonedown}">
+                <slot></slot>
+            </div>`;
+        } else {
+            return html`
             <div>
                 <slot></slot>
-            </div>`
+            </div>`;
+        }      
     }
 }
 
--- src/components/il-image-feature/il-image-feature.component.js
+++ src/components/il-image-feature/il-image-feature.component.js
@@ -7,18 +7,13 @@ class ImageFeatureComponent extends LitElement {
     return 768;
   }
 
-  static get highlightAttribute() {
-    return 'data-il-image-feature-highlight';
-  }
-  
   static get styles() {
     return styles;
   }
 
   static get properties() {
     return {
-      _compact: { default: false, type: Boolean, attribute: false, state: true },
-      linkedby: { default: false, type: String, attribute: true }
+      _compact: { default: false, type: Boolean, attribute: false, state: true }
     };
   }
 
@@ -30,72 +25,41 @@ class ImageFeatureComponent extends LitElement {
     this._compact = this.offsetWidth < ImageFeatureComponent.compactSizePixelWidth;
   }
 
-   getLinkElement() {
-     return document.getElementById(this.linkedby);
+   addPolyfillForContainerSupprt() {
+    console.debug("Image Feature: No support for @container detected: using manual resize");
+    window.addEventListener('load', this.handleResize.bind(this));
+    window.addEventListener('resize', this.handleResize.bind(this));
+   }
+
+   removePolyfillForContainerSupprt() {
+    console.debug("Image Feature: removing resize");
+    window.removeEventListener('load', this.handleResize.bind(this));
+    window.removeEventListener('resize', this.handleResize.bind(this));
    }
 
   constructor() {
     super();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
     if (!ImageFeatureComponent.hasContainerSupport()) {
-      console.debug("Image Feature: No support for @container detected: using manual resize");
-      window.addEventListener('load', this.handleResize.bind(this));
-      window.addEventListener('resize', this.handleResize.bind(this));
-    }
-  }
-
-  firstUpdated() {
-    if (this.linkedby && this.linkedby != '') {
-      let anchor = this.getLinkElement();
-      if (anchor != null) {
-        anchor.addEventListener("focus", (event) => { this.setHighlight(); });
-        anchor.addEventListener("blur", (event) => { this.removeHighlight(); });
-      } else {
-        console.debug(`Image Feature: ID ${this.linkedby} not found`);
-        this.linkedby = null;
-      }
-    }
-  }
-
-  _click(e) {
-    this.getLinkElement().click();
-  }
-
-  setHighlight() {
-    return this.setAttribute(ImageFeatureComponent.highlightAttribute, true);
-  }
-
-  removeHighlight() {
-    return this.removeAttribute(ImageFeatureComponent.highlightAttribute);
-  }
-
-  _highlight(e) {
-    this.setHighlight();
-    this.getLinkElement().focus();
-  }
-
-  _tonedown(e) {
-    this.removeHighlight();
-    this.getLinkElement().blur();
-  }
+     this.addPolyfillForContainerSupprt();
+   }
+ }
+   
+   disconnectedCallback() {
+     if (!ImageFeatureComponent.hasContainerSupport()) {
+       this.removePolyfillForContainerSupprt();
+     }
+     super.disconnectedCallback();
+   }
 
   render() {
     if (this.previousElementSibling != null && this.previousElementSibling.localName == "il-image-feature" && 
       !this.previousElementSibling.classList.contains('il-float-right') && !this.previousElementSibling.classList.contains('il-float-left') && 
       !this.classList.contains('il-float-right') && !this.classList.contains('il-float-left')) {
         this.classList.add('il-float-right');
-    }
-    if (this.linkedby && this.linkedby != '') {
-      return html`
-        <div id="container" ?compact=${this._compact} @click="${this._click}" @mouseover="${this._highlight}" @mouseout="${this._tonedown}">
-          <div id="image">
-            <slot name="image"></slot>
-          </div>
-          <div id="main">
-              <div id="content">
-                  <slot></slot>
-              </div>
-          </div>
-        </div>`;
     }
     return html`
         <div id="container" ?compact=${this._compact}>
