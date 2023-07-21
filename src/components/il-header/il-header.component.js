2ea6019f2eabfd77267d8ef010588e4d5e91db33
msharkey <mattsharkey@users.noreply.github.com>
2023-07-20 20:14:57 -0500

    New breakpoints

--- src/components/il-header/il-header.component.js
+++ src/components/il-header/il-header.component.js
@@ -5,6 +5,12 @@ import './il-header.css';
 
 class Header extends LitElement {
 
+    static get properties() {
+        return {
+            _collapsed: { type: Boolean, state: true }
+        }
+    }
+
   static get properties() {
     return {
       compact: { type: Boolean, default: false, reflect: true },
@@ -12,116 +18,122 @@ class Header extends LitElement {
     }
   }
 
-  static get styles() {
-    return styles;
-  }
-
-  // Lifecycle
-
-  constructor() {
-    super();
-    this.collapseBreakpoint = 990;
-    this.compact = false;
-    this._collapsed = false;
-    this.menuVisible = false;
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    this.listenForResize();
-  }
-
-  updated(_changedProperties) {
-    super.updated(_changedProperties);
-    if (_changedProperties.has('compact')) {
-      this.dispatchEvent(new CustomEvent('compact', {detail: { compact: this.compact }}));
+    constructor() {
+        super();
+        this.collapseBreakpoint = 990;
+        this._collapsed = false;
+        this.menuVisible = false;
+        this.handleToggleClick = this.handleToggleClick.bind(this);
     }
   }
 
-  // Getters/setters
+    connectedCallback() {
+        super.connectedCallback();
+        this.listenForResize();
+    }
 
-  get collapsed() {
-    return this._collapsed;
-  }
+    get collapsed() {
+        return this._collapsed;
+    }
 
-  getToggle() {
-    return this.shadowRoot.querySelector('button');
-  }
+    getToggle() {
+        return this.shadowRoot.querySelector('button');
+    }
 
-  handleResize() {
-    this.compact = !this.hasSpaceForFullSize();
-  }
+    handleResize() {
+        this._collapsed = this.offsetWidth < this.collapseBreakpoint;
+    }
 
-  handleToggleClick() {
-    this.toggleMenu();
-  }
+    handleToggleClick() {
+        this.toggleMenu();
+    }
 
-  hasSpaceForFullSize() {
-    return this.offsetWidth >= this.collapseBreakpoint
-  }
+    isCollapsed() {
+        return this._collapsed;
+    }
 
-  isCompact() {
-    return this.compact;
-  }
+    listenForResize() {
+        const observer = new ResizeObserver(this.handleResize.bind(this));
+        observer.observe(this);
+    }
 
-  listenForResize() {
-    const observer = new ResizeObserver(this.handleResize.bind(this));
-    observer.observe(this);
-  }
+    toggleMenu() {
+        this.menuVisible = !this.menuVisible;
+        this.updateMenuVisibilityAttribute();
+    }
 
-  toggleMenu() {
-    this.menuVisible = !this.menuVisible;
-    this.updateMenuVisibilityAttribute();
-  }
+    updateMenuVisibilityAttribute() {
+        this.setAttribute('data-il-menu-visible', this.menuVisible ? '1' : '0');
+    }
 
-  updateMenuVisibilityAttribute() {
-    this.setAttribute('data-il-menu-visible', this.menuVisible ? '1' : '0');
-  }
+    // Render
 
-  // Render
+    render() {
+        return this.isCollapsed() ? this.renderCollapsed() : this.renderUncollapsed();
+    }
 
-  render() {
-    return this.isCompact() ? this.renderCompact() : this.renderFull();
-  }
+    renderCollapsed() {
+        return html`
+            <div id="header">
+                <div id="top-stripe" aria-hidden="true"></div>
+                <div id="wordmark">
+                    <a href="https://illinois.edu/">University of Illinois Urbana-Champaign</a>
+                </div>
+                <div id="identity">
+                    <slot></slot>
+                </div>
+                <div id="menu">
+                    <button id="menu-toggle" @click=${this.handleToggleClick}>
+                        <span>Menu</span>
+                        <span class="menu-icon-bar menu-icon-bar--top"></span>
+                        <span class="menu-icon-bar menu-icon-bar--middle"></span>
+                        <span class="menu-icon-bar menu-icon-bar--bottom"></span>
+                    </button>
+                    <div id="menu-contents">
+                        <div id="links">
+                            <slot name="links"></slot>
+                        </div>
+                        <div id="search">
+                            <slot name="search"></slot>
+                        </div>
+                        <div id="navigation">
+                            <slot name="navigation"></slot>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+    }
 
-  renderBranding() {
-    return html`
-      <div id="branding">
-        <a href="https://illinois.edu/">
-          <span id="block-i">
-            <il-block-i></il-block-i>
-          </span>
-          <span id="wordmark">
-            University of Illinois Urbana-Champaign
-          </span>
-        </a>
-      </div>`
-  }
-
-  renderMenuToggle() {
-    return html`<button @click=${this.handleToggleClick}>
-      <span class="label">Menu</span>
-      <span class="menu-icon-bar menu-icon-bar--top"></span>
-      <span class="menu-icon-bar menu-icon-bar--middle"></span>
-      <span class="menu-icon-bar menu-icon-bar--bottom"></span>
-    </button>`;
-  }
-
-  renderCompact() {
-    return html`
-      <div id="header" class="compact">
-        <div id="top-stripe" aria-hidden="true"></div>
-        <div id="main">
-          <div class="content-container">
-            ${this.renderBranding()}
-            <div class="row" id="identity-and-menu-toggle">
-              <div id="identity">
-                <slot></slot>
-              </div>
-              <div id="menu-toggle">
-                ${this.renderMenuToggle()}
-              </div>
+    renderUncollapsed() {
+        return html`
+            <div id="header">
+                <div id="top-stripe" aria-hidden="true"></div>
+                <div id="main">
+                    <div class="content-container">
+                        <div id="wordmark-and-links">
+                            <div id="wordmark">
+                                <a href="https://illinois.edu/">University of Illinois Urbana-Champaign</a>
+                            </div>
+                            <div id="links">
+                                <slot name="links"></slot>
+                            </div>
+                        </div>
+                        <div id="identity-and-search">
+                            <div id="identity">
+                                    <slot></slot>
+                            </div>
+                            <div id="search">
+                                <slot name="search"></slot>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="navigation">
+                    <div class="content-container">
+                        <slot name="navigation"></slot>
+                    </div>
+                </div>
             </div>
           </div>
         </div>
