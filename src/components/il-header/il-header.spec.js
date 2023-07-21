f2827df73972dafc6994d8d91bdad935a1893eb1
msharkey <mattsharkey@users.noreply.github.com>
2023-07-20 20:14:57 -0500

    New breakpoints

--- src/components/il-header/il-header.spec.js
+++ src/components/il-header/il-header.spec.js
@@ -3,14 +3,36 @@ const AxeBuilder = require('@axe-core/playwright').default;
 
 test.use({ viewport: { width: 1280, height: 1024 } });
 
-test('empty', async ({ page }) => {
-  await page.goto('components/il-header/tests/empty.html');
-  await page.waitForTimeout(1);
-  await expect(page.getByTestId('header')).toHaveScreenshot();
-})
+test('collapses below 990px', async ({page}) => {
+  await page.setViewportSize({ width: 990, height: 800});
+  await page.goto('components/il-header/tests/header.html');
+  await expect(page.getByRole('button', { name: 'Menu' })).toBeHidden();
+  await expect(page.getByTestId('search')).toBeVisible();
 
-test('links only', async ({ page }) => {
-  await page.goto('components/il-header/tests/links-only.html');
-  await expect(page.getByTestId('header')).toHaveScreenshot();
-})
+  await page.setViewportSize({ width: 989, height: 800});
+  await expect(page.getByRole('button', { name: 'Menu' })).toBeVisible();
+  await expect(page.getByTestId('search')).toBeHidden();
+});
 
+test('campus wordmark disappears below 550px', async ({page}) => {
+  await page.setViewportSize({ width: 550, height: 800});
+  await page.goto('components/il-header/tests/header.html');
+  await expect(page.getByText('University of Illinois Urbana-Champaign')).toBeVisible();
+  await page.setViewportSize({ width: 549, height: 800});
+  await expect(page.getByText('University of Illinois Urbana-Champaign')).toBeHidden();
+});
+
+test('primary unit disappears below 550px', async ({page}) => {
+  await page.setViewportSize({ width: 550, height: 800});
+  await page.goto('components/il-header/tests/header.html');
+  await expect(page.getByText('College of Malkovich Malkovich')).toBeVisible();
+  await page.setViewportSize({ width: 549, height: 800});
+  await expect(page.getByText('College of Malkovich Malkovich')).toBeHidden();
+});
+
+test('clicking the menu toggle shows the menu', async ({page}) => {
+  await page.setViewportSize({ width: 549, height: 800});
+  await page.goto('components/il-header/tests/header.html');
+  await page.getByRole('button', { name: 'Menu'}).click();
+  await expect(page.getByTestId('search')).toBeVisible();
+})
