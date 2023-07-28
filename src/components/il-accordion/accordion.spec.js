7b6161f8951b0c631c2bb4d61bfadb223f41391d
Bryan Jonker <jonker@illinois.edu>
2023-07-28 16:34:21 -0500

    Updating accordion

--- src/components/il-accordion/accordion.spec.js
+++ src/components/il-accordion/accordion.spec.js
@@ -2,12 +2,3 @@ import { test, expect } from '@playwright/test';
 const AxeBuilder = require('@axe-core/playwright').default;
 
 test.use({ viewport: { width: 1200, height: 800 }});
-
-test.beforeEach(async({page}) => {
-    await page.goto('components/il-accordion/tests/accordion.html');
-})
-
-// TODO: Desktop and mobile screenshots
-test('it has a screenshot', async ({ page }) => {
-    await expect(page.getByTestId('component')).toHaveScreenshot();
-})
