ef8b04c03f54f74103f7b38c00564facae097074
msharkey <mattsharkey@users.noreply.github.com>
2023-07-31 10:57:28 -0500

    Fix broken card tests

--- src/components/il-accordion/accordion.spec.js
+++ src/components/il-accordion/accordion.spec.js
@@ -3,30 +3,11 @@ const AxeBuilder = require('@axe-core/playwright').default;
 
 test.use({ viewport: { width: 1200, height: 800 }});
 
-test.describe(`base`, () => {
-    test.beforeEach(async({page}) => {
-        await page.goto('components/il-accordion/tests/accordion.html');
-    })
-    test('it has a screenshot', async ({ page }) => {
-        await expect(page.getByTestId('component')).toHaveScreenshot();
-    })
-});
+test.beforeEach(async({page}) => {
+    await page.goto('components/il-accordion/tests/accordion.html');
+})
 
-test.describe(`white`, () => {
-    test.beforeEach(async({page}) => {
-        await page.goto('components/il-accordion/tests/accordion.html?theme=white');
-    })
-    test('it has a screenshot', async ({ page }) => {
-        await expect(page.getByTestId('component')).toHaveScreenshot();
-    })
-});
-
-test.describe(`fixed-width`, () => {
-    test.beforeEach(async({page}) => {
-        await page.goto('components/il-accordion/tests/accordion.html?fixed');
-    })
-
-    test('it has a screenshot', async ({ page }) => {
-        await expect(page.getByTestId('component')).toHaveScreenshot();
-    })
-});
+// TODO: Desktop and mobile screenshots
+test('it has a screenshot', async ({ page }) => {
+    await expect(page.getByTestId('component')).toHaveScreenshot();
+})
