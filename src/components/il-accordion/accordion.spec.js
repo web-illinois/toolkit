3031634919b700a2403da587b2fb9b6a5ecc5ddf
msharkey <mattsharkey@users.noreply.github.com>
2023-07-31 10:57:28 -0500

    Fix broken card tests

--- src/components/il-accordion/accordion.spec.js
+++ src/components/il-accordion/accordion.spec.js
@@ -4,12 +4,10 @@ const AxeBuilder = require('@axe-core/playwright').default;
 test.use({ viewport: { width: 1200, height: 800 }});
 
 test.beforeEach(async({page}) => {
-    await page.goto('components/accordion/accordion.html');
+    await page.goto('components/il-accordion/tests/accordion.html');
 })
 
 // TODO: Desktop and mobile screenshots
 test('it has a screenshot', async ({ page }) => {
     await expect(page.getByTestId('component')).toHaveScreenshot();
 })
-
-// TODO: White theme
--- src/components/il-page/card/card.spec.js
+++ src/components/il-page/card/card.spec.js
@@ -1,42 +1,34 @@
 import { test, expect } from '@playwright/test';
 const AxeBuilder = require('@axe-core/playwright').default;
 
-const themes = ['blue', 'orange', 'gray', 'white', 'blue-gradient'];
-
 test.use({ viewport: { width: 800, height: 600 } });
 
-themes.forEach(theme => {
-    test.describe(`theme-${theme}`, () => {
+test(`card`, async ({ page }) => {
+    await page.goto(`components/il-page/card/tests/card.html`);
+    await expect(page.getByTestId('card')).toHaveScreenshot();
+})
+test(`card accessibility`, async({page}) => {
+    await page.goto(`components/il-page/card/tests/card.html`);
+    const results = await new AxeBuilder({ page }).withRules('color-contrast').analyze();
+    expect(results.violations).toEqual([]);
+})
 
-        test(`card`, async ({ page }) => {
-            await page.goto(`components/card/tests/card.html?theme=${theme}`);
-            await expect(page.getByTestId('card')).toHaveScreenshot();
-        })
-        test(`card accessibility`, async({page}) => {
-            await page.goto(`components/card/tests/card.html?theme=${theme}`);
-            const results = await new AxeBuilder({ page }).withRules('color-contrast').analyze();
-            expect(results.violations).toEqual([]);
-        })
+test(`card with image`, async ({ page }) => {
+    await page.goto(`components/il-page/card/tests/card-with-image.html`);
+    await expect(page.getByTestId('card')).toHaveScreenshot();
+})
+test(`card with image accessibility`, async({page}) => {
+    await page.goto(`components/il-page/card/tests/card-with-image.html`);
+    const results = await new AxeBuilder({ page }).withRules('color-contrast').analyze();
+    expect(results.violations).toEqual([]);
+})
 
-        test(`card with image`, async ({ page }) => {
-            await page.goto(`components/card/tests/card-with-image.html?theme=${theme}`);
-            await expect(page.getByTestId('card')).toHaveScreenshot();
-        })
-        test(`card with image accessibility`, async({page}) => {
-            await page.goto(`components/card/tests/card-with-image.html?theme=${theme}`);
-            const results = await new AxeBuilder({ page }).withRules('color-contrast').analyze();
-            expect(results.violations).toEqual([]);
-        })
-
-        test(`link card`, async ({ page }) => {
-            await page.goto(`components/card/tests/link-card.html?theme=${theme}`);
-            await expect(page.getByTestId('card')).toHaveScreenshot();
-        })
-        test(`link card accessibility`, async({page}) => {
-            await page.goto(`components/card/tests/link-card.html?theme=${theme}`);
-            const results = await new AxeBuilder({ page }).withRules('color-contrast').analyze();
-            expect(results.violations).toEqual([]);
-        })
-
-    })
+test(`link card`, async ({ page }) => {
+    await page.goto(`components/il-page/card/tests/link-card.html`);
+    await expect(page.getByTestId('card')).toHaveScreenshot();
+})
+test(`link card accessibility`, async({page}) => {
+    await page.goto(`components/il-page/card/tests/link-card.html`);
+    const results = await new AxeBuilder({ page }).withRules('color-contrast').analyze();
+    expect(results.violations).toEqual([]);
 })
