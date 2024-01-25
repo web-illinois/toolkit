327b1c89b2fe92e9fd6307316e0ad7732b2a8df9
msharkey <mattsharkey@users.noreply.github.com>
2024-01-24 18:24:53 -0600

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? jsonReporter : [htmlReporter, jsonReporter],
+  reporter: process.env.CI ? [jsonReporter] : [htmlReporter, jsonReporter],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
