160944c95ff20e2d7be0d971dbb1a0ce5eed61c6
Bryan Jonker <jonker@illinois.edu>
2024-02-12 09:34:19 -0600

    Scrap add/remove event listener

--- src/il-accordion/il-accordion.component.js
+++ src/il-accordion/il-accordion.component.js
@@ -2,30 +2,54 @@ import {html, css, LitElement} from 'lit';
 import './il-accordion.css';
 
 export class Accordion extends LitElement {
-  constructor() {
-    super();
-    if (this.hasAttribute('data-il-allow-multiple') && this.getAttribute('data-il-allow-multiple') == 'false') {
-      this.addEventListener('expand', (e) => this.collapseOtherPanels(e.target));
-    } else {
-      this.addEventListener('expand', (e) => this.expandPanel(e.target));
-    }
-    this.addEventListener('collapse', (e) => this.collapsePanel(e.target));
-  }
-  
-  collapseOtherPanels(openedPanel) {
-    this.getPanels().forEach(panel => { if (panel != openedPanel) this.collapsePanel(panel); else this.expandPanel(panel); });
+  static get properties() {
+    return {
+        limit: { }
+    };
   }
 
-  collapsePanel(panel) {
-    panel.collapse();
+  set limit(val) {
+    this._limit = val;
+    if (this.getExpandedPanels().length > 1 && this._limit == "1") {
+      this.getExpandedPanels().forEach((panel, i) => { if (i > 0) panel.removeAttribute('data-il-state'); });
+    }
+  }
+
+  get limit() { return this._limit; }
+
+  constructor() {
+    super();
+    this._limit == '';
+    this.addEventListener('collapse', (e) => this.collapsePanel(e.target));
+    this.addEventListener('expand', (e) => this.expandPanelListener(e.target));
+  }
+  
+  expandPanelListener(panel) {
+    if (this.limit == "1") {
+      this.expandPanelAndCollapseOthers(panel);
+    } else {
+      panel.expand();
+    }
+  }
+  
+  expandPanelAndCollapseOthers(openedPanel) {
+    this.getPanels().forEach(panel => { if (panel != openedPanel) this.collapsePanel(panel); else this.expandPanel(panel); });
   }
 
   expandPanel(panel) {
     panel.expand();
   }
 
+  collapsePanel(panel) {
+    panel.collapse();
+  }
+
   getPanels() {
     return this.querySelectorAll('il-accordion-section');
+  }
+
+  getExpandedPanels() {
+    return this.querySelectorAll('il-accordion-section[data-il-state="expanded"]');
   }
 
   render() {
