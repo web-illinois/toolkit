683c9d108a278e20c1dcad57f88ee976df08fae6
Andrew Reynolds <areynold@illinois.edu>
2024-05-07 12:35:03 -0500

    add img role to wordmark element

--- src/js/components/footer/footer.component.js
+++ src/js/components/footer/footer.component.js
@@ -8,7 +8,7 @@ class Footer extends LitElement {
 
     renderWordmark() {
         return html`
-<svg class="wordmark" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 300.9 78.2" xml:space="preserve">
+<svg class="wordmark" role="img" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 300.9 78.2" xml:space="preserve">
     <title>University of Illinois Urbana-Champaign</title>
     <g class="block-i">
         <path class="block-i__outline" d="M54.2 21.1V0H0v21.1h12v36.1H0v21.1h54.2V57.2h-12V21.1h12zM42.1 60.2h9v15H3v-15h9c1.7 0 3-1.3 3-3V21.1c0-1.7-1.3-3-3-3H3V3h48.1v15h-9c-1.7 0-3 1.3-3 3v36.1c0 1.7 1.4 3.1 3 3.1"/>
